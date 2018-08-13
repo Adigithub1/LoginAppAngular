8e461cc811e0531428ad1d553db1573135f59b67
wilidga <wilidga@hotmail.com>
2018-08-12 21:14:32 -0600

    Configuracion de angulary firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyD_9QlUgbGn-Hu-uxf9PoaKNWiLFoXs9QU",
+  authDomain: "placesforfun-df235.firebaseapp.com",
+  databaseURL: "https://placesforfun-df235.firebaseio.com",
+  projectId: "placesforfun-df235",
+  storageBucket: "placesforfun-df235.appspot.com",
+  messagingSenderId: "293307244355"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyD_9QlUgbGn-Hu-uxf9PoaKNWiLFoXs9QU",
+    authDomain: "placesforfun-df235.firebaseapp.com",
+    databaseURL: "https://placesforfun-df235.firebaseio.com",
+    projectId: "placesforfun-df235",
+    storageBucket: "placesforfun-df235.appspot.com",
+    messagingSenderId: "293307244355"
+  }
 };
